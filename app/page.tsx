9c7ca6ff3ff09e76fc1842e081157a870008cbb4
SeniorIntern <tnikhil.official@gmail.com>
2023-12-22 13:42:59 +0545

    Add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import IssueChart from "./IssueChart";
 import IssueSummary from "./IssueSummary";
 import prisma from "@/prisma/client";
 import LatestIssues from "./LatestIssues";
+import { Metadata } from "next";
 
 export default async function Home() {
   const open = await prisma.issue.count({
@@ -26,4 +27,9 @@ export default async function Home() {
       <LatestIssues />
     </Grid>
   )
+}
+
+export const metadata: Metadata = {
+  title: 'Issue Tracker - Dashboard',
+  description: 'View a summary of project issues'
 }
